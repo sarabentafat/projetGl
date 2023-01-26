db7213340eb40ba6810cc49cf4f51c039ec9af56
moncefzer <monsefzermache0664@gmail.com>
2023-01-26 14:49:43 +0100

    add pipfile

--- server/app_for_u.py
+++ server/app_for_u.py
@@ -13,9 +13,6 @@ app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db=SQLAlchemy(app)
