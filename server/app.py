21b5f5d7664345d80cc9af1a27e0d62e41c79f5d
moncefzer <monsefzermache0664@gmail.com>
2023-01-27 20:47:44 +0100

    before adding auth

--- server/app.py
+++ server/app.py
@@ -1,28 +1,37 @@
 from flask import Flask
 import os
-#from flask_cors import CORS
+from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from modals import db,ma
 from routes.admin import admin
 from routes.annonces import annonces
 from routes.auth import auth
+from config import DevConfig
 
 #-----------------------app configuration------------------------
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(DevConfig)
+JWTManager(app)  #  will get the SECKRET_KEY from DevConfig
+
 db.app = app
 ma.app = app
 db.init_app(app=app)
 migrate = Migrate(app, db)
+CORS(app)
 
 
 
 if __name__ == "__main__":
   with app.app_context():
+    #! create db
     db.create_all()
+
+    #! setup routes
     app.register_blueprint(auth)
     app.register_blueprint(admin)
     app.register_blueprint(annonces)
-    app.run(debug=True)
+
+    #! run server
+    app.run()
--- server/routes/auth.py
+++ server/routes/auth.py
@@ -1,25 +1,64 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify
 from modals import db, Personnes,user_schema,users_schema
-# from flask_jwt_extended import 
+from flask_jwt_extended import create_access_token,set_access_cookies,unset_jwt_cookies,jwt_required,get_jwt_identity
 
 
 auth = Blueprint('auth',__name__,url_prefix='/api/auth')
 
 @auth.post('/register') 
-def register() :
-    return 'user created' 
+def add_user():
+  nom = request.json.get('nom', '')
+  prenom = request.json.get('prenom', '')
+  email = request.json.get('email', '')
+  tel = request.json.get('tel', '')
+  googeId = request.json.get('googleId','')
+  adresse= request.json.get('adress', '')
+  isadmin= request.json.get('isadmin', '')
+  
+  new_user = Personnes(nom=nom,prenom=prenom,googeId=googeId, email=email,tel=tel,adresse=adresse,isadmin=isadmin)
+  db.session.add(new_user)
+  db.session.commit()
+
+  return user_schema.jsonify(new_user)
 
 
 @auth.post('/login')
 def login() : 
-    return 'user login'
+    response = jsonify({"msg": "login successful"})
+    access_token = create_access_token(identity="example_user") #* will get infos from googleid
+    set_access_cookies(response, access_token)
+    return response
+
+@auth.post('/logout')
+def logout() : 
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
 
 
 @auth.get('/profile') 
+@jwt_required()
 def get_profile() :
-    return 'My profile'
+    user_id = get_jwt_identity() #* search for user in db
+    user = Personnes.query.get(id=user_id)
+    return user_schema.jsonify(user)
 
 @auth.put('/profile')
+@jwt_required()
 def update_profile() :
-    return 'Profile updated'
+  user_id = get_jwt_identity()
+  user = Personnes.query.get(id= user_id)
+  nom = request.json['nom']
 
+  # todo : add all infos here
+  user.nom = nom 
+
+
+  db.session.commit()
+  return user_schema.jsonify(user)
+
+
+@auth.put('/google/callback') 
+def login_with_google() :
+    token = create_access_token(id)
+    return 'logged with google'
